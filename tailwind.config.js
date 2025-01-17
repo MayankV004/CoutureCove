364eecc7f045918569c2cc4162d9754cba195439
MayankV004 <abcdef.msverma@gmail.com>
2025-01-17 12:27:46 +0530

    favicon and nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './views/**/*.ejs',
-    './public/javascript/**/*.js',
+    './views/**/*.ejs', // Adjust the path to your EJS templates
+    './public/**/*.html', // Include static HTML if needed
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
